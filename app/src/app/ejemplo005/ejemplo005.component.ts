e23a4663722e75f99f269580fd9fb314ec1bfada
Carlos Aleman <caleman9791@gmail.com>
2022-02-17 07:00:48 -0400

    ok

--- app/src/app/ejemplo005/ejemplo005.component.ts
+++ app/src/app/ejemplo005/ejemplo005.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ejemplo005',
-  templateUrl: './ejemplo005.component.html'
+  selector: 'app-ejemplo005',
+  templateUrl: './ejemplo005.component.html',
 })
 export class Ejemplo005 {
 
