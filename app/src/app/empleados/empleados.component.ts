b65ea3dfaffb32c3513865320788350812894ea7
Carlos Aleman <caleman971@gmail.com>
2022-02-17 23:22:32 -0500

    Nuevo componente

---
+++ app/src/app/empleados/empleados.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+selector: "app-empleados",
+templateUrl: "./empleados/empleados.component.html",
+styleUrls: ["./empleados/empleados.component.css"],
+styles: ["h1 { text-align: center; }"]
+})
+
+
+export class EmpleadosComponent { 
+constructor() {};
+
+ngOnInit(): void {};
+}
